17558b8813315972bed55afb8f9419b23127140d
Zain Tanvir <ztanvir@gridsystems.local>
2024-10-08 17:46:19 +0500

    changes

--- APCrmAPI/App_Start/WebApiConfig.cs
+++ APCrmAPI/App_Start/WebApiConfig.cs
@@ -11,8 +11,8 @@ namespace APCrmAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-            config.EnableCors(new EnableCorsAttribute("http://localhost:4200", "*", "*"));
-            config.EnableCors(new EnableCorsAttribute("https://ztanvir-1.github.io", "*", "*"));
+            //config.EnableCors(new EnableCorsAttribute("http://localhost:4200", "*", "*"));
+            //config.EnableCors(new EnableCorsAttribute("https://ztanvir-1.github.io", "*", "*"));
 
             // var cors = new EnableCorsAttribute("https://ztanvir-1.github.io", "*", "*");
             // config.EnableCors(cors);
